d7fce02f6843e1a9579df8b4fe60273742e4ea98
Keks <keks@htmlacademy.ru>
2022-02-23 12:14:55 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,24 +1,20 @@
+// Источник https://schoolsw3.com/js/js_random.php
 function getRandomNumber(min, max) {
   if (max <= min || min < 0 || max <= 0) {
-    window.console.log('Неверный диапазон');
-  } else {
-    return Math.floor(Math.random() * (max - min + 1) ) + min;
+    new Error('Неверный диапазон');
   }
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
 }
-
-// Источник https://schoolsw3.com/js/js_random.php
 
 getRandomNumber(1, 10);
 
+// Источник https://qna.habr.com/q/999157
 function getRandomFloatNumber(min, max, maxDigits = 0) {
   if (max <= min || min < 0 || max <= 0) {
-    window.console.log('Неверный диапазон');
-  } else {
-    const digitsDegree = 10 ** maxDigits;
-    return ~~((Math.random() * (max - min + 1) + min) * digitsDegree) / digitsDegree;
+    new Error('Неверный диапазон');
   }
+  const digitsDegree = 10 ** maxDigits;
+  return Math.round((Math.random() * (max - min + 1) + min) * digitsDegree) / digitsDegree;
 }
-
-// Источник https://qna.habr.com/q/999157
 
 getRandomFloatNumber(1,10,5);
